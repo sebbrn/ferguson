fad5001b451d275f399f49e9ded48a0c56be3e71
Sebastian Brune <s.brun@gmx.de>
2018-07-13 21:45:54 +0200

    Fixed Redirect_URI

--- app/controllers/oauth.controller.ts
+++ app/controllers/oauth.controller.ts
@@ -17,9 +17,8 @@ router.get('/', (req, res) => {
         uri: 'https://slack.com/api/oauth.access?code='
         + req.query.code +
         '&client_id=' + process.env.CLIENT_ID +
-        '&client_secret=' + process.env.CLIENT_SECRET,
-        // ToDo: bad_redirect_uri-Error fixen
-        // '&redirect_uri=' + process.env.REDIRECT_URI,
+        '&client_secret=' + process.env.CLIENT_SECRET +
+        '&redirect_uri=' + process.env.REDIRECT_URI,
         method: 'GET'
     };
     console.log(options);
