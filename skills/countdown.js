57a21e06eb677425fed185ffab824236cd370bc9
Johannes Hollekamp <johannes.hollekamp@zweitag.de>
2019-08-21 16:07:36 +0200

    humanize left duration in countdown component

--- skills/countdown.js
+++ skills/countdown.js
@@ -1,3 +1,5 @@
+const humanizeDuration = require('humanize-duration')
+
 module.exports = function (controller) {
 
     // add event handlers to controller
@@ -87,11 +89,11 @@ function daysLeft(date) {
     if (!(date instanceof Date)) {
         date = new Date(date);
     }
-    const days = countDays(date);
+    const humanized = humanize(date);
     if (days < 15) {
-        return `_Hooray!_ There are only *${days}* days left! :star-struck:`;
+        return `_Hooray!_ Only *${humanized}* left! :star-struck:`;
     } else {
-        return `There are *${days}* days left! Wow, that's still a loooong time :face_with_rolling_eyes:`;
+        return `*${humanized}* left! Wow, that's still a loooong time :face_with_rolling_eyes:`;
     }
 }
 
@@ -100,6 +102,6 @@ function daysLeft(date) {
  * @param date
  * @return {number}
  */
-function countDays(date) {
-    return Math.floor((date - new Date()) / (1000 * 60 * 60 * 24));
+function humanize(date) {
+    return humanizeDuration(date - new Date(), { units: ['y', 'm', 'w', 'd'] });
 }
